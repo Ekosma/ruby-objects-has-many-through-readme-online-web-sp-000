1d72b9a34bd093fefa54a2a47d5fe379bcd123c1
Evelyn Kosma <evelyn.a.kosma@gmail.com>
2021-04-03 21:03:36 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -21,7 +21,14 @@ class Customer
   def meals
     Meal.all.select do |meal|
       meal.customer == self
+    end
   end
-end
  
+  def best_tipper
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+ 
+  best_tipped_meal.customer
+  end
 end
